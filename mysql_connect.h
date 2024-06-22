0049bb196559520a664f5cd4e8ba5f0f8c2f26d9
IOleg-sus <guchkooleg7@gmail.com>
2024-06-22 11:39:20 +0300

    working template

--- mysql_connect.h
+++ mysql_connect.h
@@ -9,10 +9,10 @@ struct connection_details
     const char* database;
 };
 
-MYSQL* mysql_connection_setup(connection_details mylibrary_details)
+MYSQL* mysql_connection_setup(connection_details *mylibrary_details)
 {
     MYSQL* connection = mysql_init(NULL);
-    if (!mysql_real_connect(connection , mylibrary_details.server , mylibrary_details.user , mylibrary_details.password , mylibrary_details.database , 0 , NULL , 0 ))
+    if (!mysql_real_connect(connection , mylibrary_details->server , mylibrary_details->user , mylibrary_details->password , mylibrary_details->database , 0 , NULL , 0 ))
     {
         std::cout << "Connection failed " << mysql_error(connection) << std::endl;
         exit(1);
--- mysqlversion_vs.cpp
+++ mysqlversion_vs.cpp
@@ -5,7 +5,6 @@
 //loading sql library
 
 #include <Windows.h>
-
 //structs
 #include "mysql_connect.h"
 
@@ -32,7 +31,30 @@ int main(int argc , const char *argv[])
     MYSQL* conn;
     MYSQL_ROW row;
     MYSQL_RES* res;
-    //conn = mysql_init(NULL);
-    std::cout << "Working debug" << std::endl;
+   
+    connection_details MySQLData;
+
+    MySQLData.database = "books";
+    MySQLData.server = "localhost";
+
+    MySQLData.user = "root";
+    MySQLData.password = "";
+
+    conn = mysql_connection_setup(&MySQLData);
+    res = mysql_execute_query(conn, "select * from books.library;");
+
+    std::cout << "Display all information" << std::endl;
+
+    while ((row = mysql_fetch_row(res)) != NULL)
+    {
+        std::cout << row[0] << "|" << row[1] << "|" << row[2] << "|" << row[3] << std::endl;
+
+        std::cout << std::endl;
+    }
+
+    mysql_free_result(res);
+    mysql_close(conn);
+
+    
 
 }

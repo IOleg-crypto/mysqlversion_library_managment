2e76c34556a73da61a36039631fcbfe84727493a
IOleg-sus <guchkooleg7@gmail.com>
2024-06-25 13:43:16 +0300

    refactoring code

--- library_table.sql
+++ library_table.sql
@@ -1,1 +1,2 @@
-select * from library;
+alter table library
+modify 
--- mysqlversion_vs.cpp
+++ mysqlversion_vs.cpp
@@ -1,17 +1,16 @@
 // mysqlversion_vs.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
 #include <thread>
 #include <sstream>
-//loading sql library
-
+#include <cstdio>
+// loading sql library
 #include <Windows.h>
-//structs sql
+// structs sql
 #include "mysql_connect.h"
-//functionality header
+// functionality header
 #include "Functionatlity.h"
-//add class SQLibrary
+// add class SQLibrary
 
 #define DEBUG_MEMORY 0
 
@@ -29,113 +28,144 @@ void operator delete(void* ptr)
 }
 #endif
 
-//global variables (FOR SQL)
+// global variables (FOR SQL)
 MYSQL* conn;
 MYSQL_ROW row;
 MYSQL_RES* res;
 
+void displayMenu() {
+    drawline(130, '-');
+    std::cout << "\t\t\t\t\t\t\tWelcome to our library";
+    std::cout << std::endl;
+    std::cout << "\t\t\t\tSelection menu" << std::endl;
+    std::cout << "1. Add new book to library" << std::endl;
+    std::cout << "2. Exit" << std::endl;
+    std::cout << "3. Show all books in library" << std::endl;
+    std::cout << "4. Take book from library" << std::endl;
+    drawline(50, '-');
+    std::cout << "Choose the option: ";
+}
+
+void addBook() {
+    int shelf_number, book_id;
+    std::string book_name, author_book, datatime;
+
+    std::cout << "Write book id: ";
+    std::cin >> book_id;
+    std::cout << "Write shelf_id: ";
+    std::cin >> shelf_number;
+    std::cin.ignore(); // Ignore remaining newline character
+    std::cout << "Enter the name of the book: ";
+    std::getline(std::cin, book_name);
+    std::cout << "Enter the author of the book: ";
+    std::getline(std::cin, author_book);
+    std::cout << "Enter the datetime (YYYY-MM-DD ): ";
+    std::getline(std::cin, datatime);
+
+    char parameter_table[512];
+    snprintf(parameter_table, sizeof(parameter_table),
+        "INSERT INTO library VALUES (%d, %d, '%s', '%s', '%s')",
+        shelf_number, book_id, book_name.c_str(), author_book.c_str(), datatime.c_str());
+
+    if (mysql_real_query(conn, parameter_table, strlen(parameter_table)) != 0) {
+        std::cerr << "INSERT INTO library failed. Error: " << mysql_error(conn) << "\n";
+    }
+    else {
+        std::cout << "Record inserted successfully.\n";
+    }
+}
+
+void showAllBooks() {
+    res = mysql_execute_query(conn, "SELECT * FROM library;");
+    if (!res) {
+        std::cerr << "SELECT * FROM library failed. Error: " << mysql_error(conn) << "\n";
+        return;
+    }
+
+    int num_fields = mysql_num_fields(res);
+    while ((row = mysql_fetch_row(res))) {
+        std::ostringstream oss;
+        for (int i = 0; i < num_fields; i++) {
+            if (i > 0) {
+                oss << " | ";
+            }
+            oss << (row[i] ? row[i] : "NULL");
+        }
+        std::cout << oss.str() << std::endl;
+    }
+    mysql_free_result(res);
+}
+
+void takeBook() {
+    int book_id;
+    std::cout << "Enter the ID of the book to take: ";
+    std::cin >> book_id;
+
+    char query[256];
+    snprintf(query, sizeof(query), "DELETE FROM library WHERE book_id = %d", book_id);
+
+    if (mysql_real_query(conn, query, strlen(query)) != 0) {
+        std::cerr << "DELETE FROM library failed. Error: " << mysql_error(conn) << "\n";
+    }
+    else {
+        std::cout << "Book with ID " << book_id << " has been taken (deleted) successfully.\n";
+    }
+}
+
 int main(int argc, const char* argv[])
 {
-    //main variables
-    int commutator = 0;
-    char operator_return = '\0';
-    int shelf_number = 0;
-    int book_id = 0;
-    std::string book_name;
-    std::string author_book;
-    std::string datatime;
-   
     connection_details MySQLData;
-
-    //template to connect sql server
     MySQLData.database = "books";
     MySQLData.server = "localhost";
     MySQLData.user = "root";
     MySQLData.password = "";
 
-    //first action
-    drawline(130 , '-');
-    std::cout << "\t\t\t\t\t\t\tWelcome to our library";
-    std::cout << std::endl;
-    std::cout << "\t\t\t\tSelection menu" << std::endl;
-    std::cout << "1 . Add new book to library" << std::endl;
-    std::cout << "2 . Exit" << std::endl;
-    std::cout << "3 . Show all books in library" << std::endl;
-    drawline(50, '-');
-    std::cout << "Choose the option : ";
-    std::cin >> commutator;
-  
-
     conn = mysql_connection_setup(MySQLData);
-    res = mysql_execute_query(conn, "select * from library;");
-    //initialize
-    int num_fields = mysql_num_fields(res);
-    switch (commutator)
-    {
-    case 1:
-        std::cout << "Write book id : ";
-        std::cin >> book_id;
-        std::cout << "Write shelf_id : ";
-        std::cin >> shelf_number;
-        std::cout << "Enter the name of the book : ";
-        std::cin.ignore(1, '\t');
-        std::getline(std::cin, book_name);
-        std::cout << "Enter the author of the book : ";
-        std::cin.ignore(1, '\t');
-        std::getline(std::cin, author_book);
-        std::cout << "Enter the name of the book : ";
-        std::cin.ignore(1, '\t');
-        std::getline(std::cin, datatime);
-
-        //access to sql query
-        addInformationBook(shelf_number, book_id, book_name, author_book, datatime , conn); // TODO : need fix connection
-        break;
-    case 2:
-        system("CLS");
-        std::cout << "Goodbye! Catch you later" << std::endl;
-        break;
-    case 3:
-        std::cout << "Display all information" << std::endl;
-        while ((row = mysql_fetch_row(res))) {
-            std::ostringstream oss;
-            for (int i = 0; i < num_fields; i++) {
-                if (i > 0) {
-                    oss << "|" << "\t";
-                }
-                oss << (row[i] ? row[i] : "NULL");
-            }
-            std::cout << oss.str() << std::endl;
-        }
-        mysql_free_result(res);
-        mysql_close(conn);
-       
+    if (!conn) {
+        std::cerr << "Connection to database failed. Error: " << mysql_error(conn) << "\n";
+        return EXIT_FAILURE;
     }
 
+    bool running = true;
+    while (running) {
+        displayMenu();
 
-    std::cout << "Do you want come back at the beginning (y/n) :  ";
-    std::cin >> operator_return;
+        int commutator;
+        std::cin >> commutator;
 
-    switch (operator_return)
-    {
-    case 'y':
-        system("CLS");
-        return main(argc, argv);
-        break;
-    case 'n':
-        std::cout << "See you next time! Goodbye";
-        exit(0);
-        break;
-    default:
-        std::cout << "Wrong input!" << std::endl;
-
-        while (operator_return != 'n' && operator_return != 'y')
+        switch (commutator)
         {
-            std::cout << " You've entered an invalid response. Please only select 'n', 'y'." << std::endl;
-            std::cout << "Do you want come back at the beginning (y/n) :  ";
+        case 1:
+            addBook();
+            break;
+        case 2:
+            std::cout << "Goodbye! Catch you later" << std::endl;
+            running = false;
+            break;
+        case 3:
+            showAllBooks();
+            break;
+        case 4:
+            showAllBooks();
+            takeBook();
+            break;
+        default:
+            std::cout << "Invalid option. Please try again." << std::endl;
+            break;
+        }
+
+        if (running) {
+            char operator_return;
+            std::cout << "Do you want to come back to the beginning (y/n): ";
             std::cin >> operator_return;
+
+            if (operator_return == 'n') {
+                std::cout << "See you next time! Goodbye" << std::endl;
+                running = false;
+            }
         }
     }
-    
-    
 
+    mysql_close(conn);
+    return EXIT_SUCCESS;
 }

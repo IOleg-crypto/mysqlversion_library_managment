bc68742e64c40d55ff691054278397966f4da00a
IOleg-sus <guchkooleg7@gmail.com>
2024-06-25 18:39:56 +0300

    refactoring code(add check)

--- mysqlversion_vs.cpp
+++ mysqlversion_vs.cpp
@@ -4,6 +4,7 @@
 #include <thread>
 #include <sstream>
 #include <cstdio>
+#include <iomanip>
 // loading sql library
 #include <Windows.h>
 // structs sql
@@ -12,9 +13,9 @@
 #include "Functionatlity.h"
 // add class SQLibrary
 
-#define DEBUG_MEMORY 0
+#define DEBUG_MEMORY 1
 
-#if DEBUG_MEMORY == 1 // DEBUG_MEMORY == 0 (tracking allocated memory)
+#if DEBUG_MEMORY == 0 // DEBUG_MEMORY == 0 (tracking allocated memory)
 void* operator new(size_t size)
 {
     std::cout << "Allocating " << size << " bytes of memory";
@@ -33,38 +34,50 @@ MYSQL* conn;
 MYSQL_ROW row;
 MYSQL_RES* res;
 
-void displayMenu() {
-    drawline(130, '-');
-    std::cout << "\t\t\t\t\t\t\tWelcome to our library";
-    std::cout << std::endl;
-    std::cout << "\t\t\t\tSelection menu" << std::endl;
-    std::cout << "1. Add new book to library" << std::endl;
-    std::cout << "2. Exit" << std::endl;
-    std::cout << "3. Show all books in library" << std::endl;
-    std::cout << "4. Take book from library" << std::endl;
-    drawline(50, '-');
-    std::cout << "Choose the option: ";
-}
-
-void addBook() {
+void addBook(MYSQL* conn) {
     int shelf_number, book_id;
     std::string book_name, author_book, datatime;
 
     std::cout << "Write book id: ";
     std::cin >> book_id;
-    std::cout << "Write shelf_id: ";
+    std::cout << "Write shelf_number: ";
     std::cin >> shelf_number;
     std::cin.ignore(); // Ignore remaining newline character
     std::cout << "Enter the name of the book: ";
     std::getline(std::cin, book_name);
     std::cout << "Enter the author of the book: ";
     std::getline(std::cin, author_book);
-    std::cout << "Enter the datetime (YYYY-MM-DD ): ";
+    std::cout << "Enter the datetime (YYYY-MM-DD): ";
     std::getline(std::cin, datatime);
 
+    // Check if the book_id already exists in the table
+    char check_query[256];
+    snprintf(check_query, sizeof(check_query), "SELECT * FROM library WHERE name_id = %d", book_id);
+
+    if (mysql_real_query(conn, check_query, strlen(check_query)) != 0) {
+        std::cerr << "Check book_id failed. Error: " << mysql_error(conn) << "\n";
+        return;
+    }
+
+    MYSQL_RES* check_res = mysql_store_result(conn);
+    if (!check_res) {
+        std::cerr << "Check book_id result failed. Error: " << mysql_error(conn) << "\n";
+        return;
+    }
+
+    MYSQL_ROW check_row = mysql_fetch_row(check_res);
+    int count = atoi(check_row[0]);
+    mysql_free_result(check_res);
+
+    if (count > 0) {
+        std::cout << "Book with ID " << book_id << " already exists. Please use a different ID.\n";
+        return;
+    }
+
+    // Proceed with insertion if the book_id does not exist
     char parameter_table[512];
     snprintf(parameter_table, sizeof(parameter_table),
-        "INSERT INTO library VALUES (%d, %d, '%s', '%s', '%s')",
+        "INSERT INTO library (shelf_number, book_id, book_name, author_book, datetime) VALUES (%d, %d, '%s', '%s', '%s')",
         shelf_number, book_id, book_name.c_str(), author_book.c_str(), datatime.c_str());
 
     if (mysql_real_query(conn, parameter_table, strlen(parameter_table)) != 0) {
@@ -75,7 +88,7 @@ void addBook() {
     }
 }
 
-void showAllBooks() {
+void showAllBooks(MYSQL_RES* res) {
     res = mysql_execute_query(conn, "SELECT * FROM library;");
     if (!res) {
         std::cerr << "SELECT * FROM library failed. Error: " << mysql_error(conn) << "\n";
@@ -83,26 +96,37 @@ void showAllBooks() {
     }
 
     int num_fields = mysql_num_fields(res);
+
+    // Print table header
+    std::cout << std::left << std::setw(15) << "Shelf Number"
+        << std::left << std::setw(10) << "Book ID"
+        << std::left << std::setw(30) << "Book Name"
+        << std::left << std::setw(30) << "Author Book"
+        << std::left << std::setw(20) << "Datetime" << std::endl;
+    drawline(105, '-'); // Adjust this according to the total width of the table
+
+    // Print table rows
     while ((row = mysql_fetch_row(res))) {
-        std::ostringstream oss;
         for (int i = 0; i < num_fields; i++) {
-            if (i > 0) {
-                oss << " | ";
-            }
-            oss << (row[i] ? row[i] : "NULL");
+            if (i == 0) std::cout << std::left << std::setw(15) << (row[i] ? row[i] : "NULL");
+            else if (i == 1) std::cout << std::left << std::setw(10) << (row[i] ? row[i] : "NULL");
+            else if (i == 2) std::cout << std::left << std::setw(30) << (row[i] ? row[i] : "NULL");
+            else if (i == 3) std::cout << std::left << std::setw(30) << (row[i] ? row[i] : "NULL");
+            else if (i == 4) std::cout << std::left << std::setw(20) << (row[i] ? row[i] : "NULL");
         }
-        std::cout << oss.str() << std::endl;
+        std::cout << std::endl;
     }
     mysql_free_result(res);
+    drawline(105, '-'); // Adjust this according to the total width of the table
 }
 
-void takeBook() {
+void takeBook(MYSQL* conn) {
     int book_id;
     std::cout << "Enter the ID of the book to take: ";
     std::cin >> book_id;
 
     char query[256];
-    snprintf(query, sizeof(query), "DELETE FROM library WHERE book_id = %d", book_id);
+    snprintf(query, sizeof(query), "DELETE FROM library WHERE name_id = %d", book_id);
 
     if (mysql_real_query(conn, query, strlen(query)) != 0) {
         std::cerr << "DELETE FROM library failed. Error: " << mysql_error(conn) << "\n";
@@ -136,18 +160,18 @@ int main(int argc, const char* argv[])
         switch (commutator)
         {
         case 1:
-            addBook();
+            addBook(conn);
             break;
         case 2:
             std::cout << "Goodbye! Catch you later" << std::endl;
             running = false;
             break;
         case 3:
-            showAllBooks();
+            showAllBooks(res);
             break;
         case 4:
-            showAllBooks();
-            takeBook();
+            showAllBooks(res);
+            takeBook(conn);
             break;
         default:
             std::cout << "Invalid option. Please try again." << std::endl;
